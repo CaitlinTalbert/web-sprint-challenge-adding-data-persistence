acfd11cd1aafca65ac99a27f18f83f0325b2f63c
CaitlinTalbert <92820628+CaitlinTalbert@users.noreply.github.com>
2022-04-22 07:36:40 -0500

    server running

--- api/server.js
+++ api/server.js
@@ -1,1 +1,15 @@
-// build your server here and require it from index.js
+const express = require("express");
+
+//routers to api here
+
+const server = express();
+
+server.use(express.json());
+
+//servers here for apis
+
+server.use("*", (req, res, next) => {
+  res.json({ api: "up" });
+});
+
+module.exports = server;
